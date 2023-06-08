4d36fbcedbe4db744c3cc710bb33f71e803854c4
Soybean <honghuangdc@gmail.com>
2023-06-09 00:03:06 +0800

    feat(release): release thin branch

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -2,13 +2,9 @@ import type { PluginOption } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import unocss from '@unocss/vite';
-import progress from 'vite-plugin-progress';
 import pageRoute from '@soybeanjs/vite-plugin-vue-page-route';
 import unplugin from './unplugin';
 import mock from './mock';
-import visualizer from './visualizer';
-import compress from './compress';
-import pwa from './pwa';
 
 /**
  * vite插件
@@ -24,19 +20,9 @@ export function setupVitePlugins(viteEnv: ImportMetaEnv): (PluginOption | Plugin
     vueJsx(),
     ...unplugin(viteEnv),
     unocss(),
-    mock(viteEnv),
-    progress()
+    mock(viteEnv)
   ];
 
-  if (viteEnv.VITE_VISUALIZER === 'Y') {
-    plugins.push(visualizer as PluginOption);
-  }
-  if (viteEnv.VITE_COMPRESS === 'Y') {
-    plugins.push(compress(viteEnv));
-  }
-  if (viteEnv.VITE_PWA === 'Y' || viteEnv.VITE_VERCEL === 'Y') {
-    plugins.push(pwa());
-  }
   if (viteEnv.VITE_SOYBEAN_ROUTE_PLUGIN === 'Y') {
     plugins.push(pageRoute());
   }
